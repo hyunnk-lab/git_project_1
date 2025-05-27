467ad4ed5122ff0d3a09ee96d3fdd5fa40676d4a
hyunnk-lab <hyun98hc@naver.com>
2025-05-27 17:47:43 +0900

    계산기 코드 진짜 최종 수정

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("Fuck you")
+from add import add
+from subtract import subtract
+from multiply import multiply
+from divide import divide
+
+def calculator():
+    a = float(input("1st number: "))
+    b = float(input("2nd number: "))
+    op = input("연산자 (+, -, *, /): ")
+
+    operations = {
+        '+': add,
+        '-': subtract,
+        '*': multiply,
+        '/': divide
+    }
+
+    if op in operations:
+        try:
+            result = operations[op](a, b)
+            print("result:", result)
+        except Exception as e:
+            print("오류:", e)
+    else:
+        print("invalid operation.")
+
+if __name__ == "__main__":
+    calculator()
